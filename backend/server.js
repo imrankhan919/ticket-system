61c52522f888c2e603f581fc168a58d5b64bc9a2
imrankhan919 <imrankhan919.ik@gmail.com>
2022-02-14 13:01:51 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('server...');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000 
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.status(201).json({message : 'Welcome to Eskills'})
+})
+
+
+app.use('/api/users',require('./routes/userRoutes'))
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at PORT: ${PORT}`);
+})
