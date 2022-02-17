703fe1667f801030b0ee8f9df8705a5606f7db5b
imrankhan919 <imrankhan919.ik@gmail.com>
2022-02-17 16:36:08 +0530

    Third Commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,23 @@
+
+
 const registerUser = (req,res) =>{
-    res.send('Register Route')
+    const {name,email,password} = req.body
+    //Validations
+    // if(!name || !email || !password){
+    //     return res.status(400).json({msg:"Please Include All Fields"})
+    // }
+
+     if(!name || !email || !password){
+       res.status(400)
+       throw new Error('Please Include All Fields!')
+    } 
+    console.log(`Your name is ${name} `);
+    
 }
 
 const loginUser = (req,res) =>{
     res.send('Login Route')
-}
+} 
 
 
 module.exports = { 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000 
+const PORT = process.env.PORT || 5000 
 const app = express()
 
+//Setup Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Use Error Middleware
+app.use(errorHandler)
+
+
+
+//Default Route
 app.get('/',(req,res)=>{
     res.status(201).json({message : 'Welcome to Eskills'})
 })
 
 
+//User Router
 app.use('/api/users',require('./routes/userRoutes'))
 
 app.listen(PORT,()=>{
